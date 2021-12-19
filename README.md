e1c1741a91687601c2461c781fb2bcf0cd48d573
youngchan <kimynam1@navercom>
2021-12-19 17:11:53 +0900

    세팅 중

--- README.md
+++
@@ -1,1 +0,0 @@
-리액트 기본 스켈레톤 with airbnb-config-eslint
--- src/apis/Constants.js
+++
@@ -1,3 +0,0 @@
-export const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://121.129.37.212:7430' : 'https://api.keepsplan.co.kr';
-export const PROJECT_NAME = 'archi-shome';
-export const S3Bucket = 'archi-shome';
